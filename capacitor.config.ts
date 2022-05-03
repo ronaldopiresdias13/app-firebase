654a5c84e94222f2338ef4e16d2e9fb0584a7b35
Ronaldo Pires Dias <ronaldo.pires@armac.com.br>
2022-05-03 14:44:58 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionic-firestore',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
